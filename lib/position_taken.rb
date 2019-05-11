eaff0b06d384d57634a7b51a97b6397ae189ad36
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-11 19:33:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  is_taken? == "nil"
+  taken == "nil"
   if (board[index] == " " || board[index] == " " || board[index] == " ")
+    taken = false
+  else
+    taken = true
   end
